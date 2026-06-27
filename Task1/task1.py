a7bc039c74c04f729f59e8771c12f465f2878a6e
oggusrinidhi <voggusrinidhi@gmail.com>
2026-06-27 23:13:15 +0530

    Task1-variables

---
+++ Task1/task1.py
@@ -0,0 +1,15 @@
+pi = 22 / 7
+
+print("Value of pi:", pi)
+
+print("Data type of pi:", type(pi))
+print("\nQuestion 2:")
+print("'for' cannot be used as a variable name because 'for' is a reserved keyword in Python.")
+principal = 50000
+rate = 8
+time = 3
+
+simple_interest = (principal * rate * time) / 100
+
+print("\nQuestion 3:")
+print("Simple Interest =", simple_interest)
